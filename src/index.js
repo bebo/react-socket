597153e70f8bc280047e24b53f75924b22197f2f
Jonas Daniels <jonas.daniels@outlook.com>
2017-02-27 04:35:24 -0800

    now

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 /**
  * Created by jonas on 2/25/17.
  */
-var Source = require('./Source');
-var Action = require('./Action');
-var withSocket = require('./withSocket');
+import Source from './Source';
+import Action from './Action';
+import withSocket from './withSocket';
 
 
 export {
